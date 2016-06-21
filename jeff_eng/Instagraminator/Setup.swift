a830e40a3aa231ace28d5af935c24339ff724d23
Jeff Eng <jeff@jeffeng.me>
2016-06-21 16:23:24 -0700

    added the Protocol file

---
+++ jeff_eng/Instagraminator/Setup.swift
@@ -0,0 +1,16 @@
+//
+//  Setup.swift
+//  Instagraminator
+//
+//  Created by Jeffrey Eng on 6/21/16.
+//  Copyright © 2016 Jeffrey Eng. All rights reserved.
+//
+
+import Foundation
+
+protocol Setup {
+    
+    func setup()
+    func setupAppearance()
+    
+}
