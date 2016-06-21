c86257d81515d2b31d216d857fd85f068b404688
Jeff Eng <jeff@jeffeng.me>
2016-06-21 13:45:00 -0700

    created ImageViewController and Post files

---
+++ jeff_eng/Instagraminator/Post.swift
@@ -0,0 +1,19 @@
+//
+//  Post.swift
+//  Instagraminator
+//
+//  Created by Jeffrey Eng on 6/21/16.
+//  Copyright © 2016 Jeffrey Eng. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    
+    let image : UIImage
+    
+    init(image: UIImage)
+    {
+        self.image = image
+    }
+}
